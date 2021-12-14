bcb155958fdc4f8ec2f21af6bbcf1c637a109949
Trevor Croxson <trevor@vedadata.com>
2021-12-13 21:31:57 -0600

    refactor(parsers/tin): update parsing behavior

--- ironhide/parsers/tin.py
+++ ironhide/parsers/tin.py
@@ -1,7 +1,6 @@
 """Parse TIN Number"""
 from __future__ import annotations
 
-from contextlib import suppress
 from typing import Dict, List, Union, Optional, Any
 
 from optimus.models.tin import TINModel
@@ -9,28 +8,57 @@ from optimus.parsers.parser import Parser
 from optimus.models.model import Model
 
 
+class Counter:
+    """Count numbers of things"""
+
+    def __init__():
+        """init"""
+        self.counts = {}
+
+    def add_item(self, item):
+        """add an item to the counter"""
+        if item not in self.counts:
+            self.counts[item] = 1
+        else:
+            self.counts[item] = self.counts[item] + 1
+
+    def get_counts(self):
+        """
+        get all counts
+        """
+        return self.counts
+
+
 class TINParser(Parser):
     """TIN Parser."""
 
     @classmethod
-    def parse(cls, raw: Union[str, Dict[str, str]]) -> Model:
+    def parse(cls, raw: Union[str, List[str]]) -> Model:
         """
         Parse input.
 
-        Atributes
-        ---------
+        Attributes
+        ----------
         raw
         """
 
         try:
-            for k, v in raw.items():
+            for item in raw:
+                # Bad TINs:
                 for i in ("111111111", "333333333", "666666666", "123456789"):
-                    if i == v:
+                    if i == item:
                         raise Exception
-                elif not k:
-                    raise Exception
-                else:
-                    return TINModel(**v)
+                counter = Counter()
+                counter.add_item(item)
+
+            all_counts = counter.get_counts()
+
+            models = []
+            for k, v in all_counts:
+                models.append(TINModel(tin=k, count=v))
+
+            return models
 
         except Exception as e:
-            raise e
+            print("An error occurred")
+            return
