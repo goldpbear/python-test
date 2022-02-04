751c155b3eb1048f583de08c5befcc4b29ad2b97
Stephen Jaeb <sdjaeb@gmail.com>
2022-02-04 16:02:15 -0600

    Added a test that would process the provided set of SSNs

--- ironhide/parsers/tin.py
+++ ironhide/parsers/tin.py
@@ -7,6 +7,11 @@ from ironhide.models.tin import TINModel
 from ironhide.parsers.parser import Parser
 from ironhide.models.model import Model
 
+import re
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class Counter:
     """Count numbers of things"""
@@ -33,32 +38,76 @@ class TINParser(Parser):
     """TIN Parser."""
 
     @classmethod
-    def parse(cls, raw: Union[str, List[str]]) -> Model:
-        """
-        Parse input.
+    def is_valid_tin(cls, tin):
+        """Validates a TIN.  May be a SSN or ITIN.  The rules for validation are:
+            Valid TIN - ITIN: /^9\d{2}-?((5[0-9]|6[0-5])|(8[3-8])|(9[0-2])|(9[4-9]))-?\d{4}$/
+            Valid TIN - SSN: /^((?!666|000)[0-8][0-9\_]{2}\-(?!00)[0-9\_]{2}\-(?!0000)[0-9\_]{4})*$/
+            Invalid TIN - SSN: 111111111, 333333333, 666666666, 123456789 (666666666 is checked in the regex)
+            Invalid TIN - SSN: Area Numbers (first 3 digits) - 000, 666 (900 – 999 could be a valid ITIN))
+            Invalid TIN - SSN: Group Numbers (4th / 5th digits) - 00
+            Invalid TIN - SSN: Serial Numbers (last 4 digits) - 0000
 
-        Attributes
-        ----------
-        raw
+        Args:
+        tin: A string representing a Tax Identification Number.
+
+        Returns:
+        True if passed TIN is valid, False if not
+
+        Raises:
+        Exception: If there is an issue validating the tin
+        """
+
+        ssn_regex = re.compile(r'^((?!666|000)[0-8][0-9\_]{2}\-(?!00)[0-9\_]{2}\-(?!0000)[0-9\_]{4})*$')
+        itin_regex = re.compile(r'^9\d{2}-?((5[0-9]|6[0-5])|(8[3-8])|(9[0-2])|(9[4-9]))-?\d{4}$')
+        invalid_ssns_explicit = ['111-11-1111', '333-33-3333', '123-45-6789']
+
+        try:
+            if not tin:
+                return False
+            if (re.match(ssn_regex, tin) and (tin not in invalid_ssns_explicit)) or re.match(itin_regex, tin):
+                return True
+        except Exception as e:
+            print(f'An error occurred validating tin {tin}: {e}')
+            logger.error(f'An error occurred validating tin {tin}: {e}')
+            return False
+
+        return False
+
+    @classmethod
+    def parse(cls, raw: Union[str, List[str]]) -> Model:
+        """Parse input.
+
+        Args:
+        raw: A string or list representing a TIN and type of TIN.
+
+        Returns:
+        A list of valid TINModels.
+
+        Raises:
+        Exception: If there is an issue parsing the input
         """
 
         try:
-            for i in raw:
-                # Bad TINs:
-                for j in ("111111111", "333333333", "666666666", "123456789"):
-                    if j == i:
-                        raise Exception
-                counter = Counter()
-                counter.add_item(i)
+            counter = Counter()
 
-            all_counts = counter.get_counts()
+            for i in raw:
+                if len(i) > 0:
+                    tin = i[0]
+                    valid_tin = cls.is_valid_tin(tin)
+
+                    if valid_tin:
+                        counter.add_item(tin)
+                    else:
+                        print(f'Invalid TIN: {tin}')
+                        logger.info(f'Invalid TIN: {tin}')
 
             models = []
-            for k, v in all_counts:
+            for k, v in counter.get_counts().items():
                 models.append(TINModel(tin=k, count=v))
 
             return models
 
         except Exception as e:
-            print("An error occurred")
+            print(f'An error occured parsing TINs: {e}')
+            logger.error(f'An error occured parsing TINs: {e}')
             return
--- tests/unit/parsers/tin/test_tin.py
+++ tests/unit/parsers/tin/test_tin.py
@@ -18,6 +18,17 @@ INVALID_NUMS = [[f"{_}{_+3}", "INVALID"] for _ in range(50)]
 
 
 @pytest.mark.parametrize("tin, tin_type", SSNS)
+# @pytest.mark.skip(reason="not testing right now")
 def test_tin(tin, tin_type):
-    # TODO: add tests
-    assert False
+    
+    assert TINParser.is_valid_tin(tin) is True
+    assert tin_type == "SSN"
+    
+    parsed_list_of_valid_ssns = TINParser.parse(SSNS)
+
+    assert parsed_list_of_valid_ssns
+    assert len(parsed_list_of_valid_ssns) == 50
+
+    for tinmodel in parsed_list_of_valid_ssns:
+        assert type(tinmodel).__name__ is "TINModel"
+        assert tinmodel.count > 0
